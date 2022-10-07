d19cd3c4e42b2a6b5aa81f2ab054715218cd691a
Ermat <ealymbaev@gmail.com>
2022-10-07 12:14:41 +0600

    Organize and update source code

--- Package.swift
+++ Package.swift
@@ -3,23 +3,27 @@
 import PackageDescription
 
 let package = Package(
-    name: "ScanQrKit",
-    defaultLocalization: "en",
-    platforms: [
-        .iOS(.v13),
-    ],
-    products: [
-        .library(
-            name: "ScanQrKit",
-            targets: ["ScanQrKit"]),
-    ],
-    dependencies: [
-        .package(url: "https://github.com/horizontalsystems/ThemeKit.git", .upToNextMajor(from: "1.0.0")),
-        .package(url: "https://github.com/horizontalsystems/ComponentKit.git", .upToNextMajor(from: "1.0.0")),
-    ],
-    targets: [
-        .target(
-            name: "ScanQrKit",
-            dependencies: ["ThemeKit", "ComponentKit"]),
-    ]
+        name: "ScanQrKit",
+        defaultLocalization: "en",
+        platforms: [
+            .iOS(.v13),
+        ],
+        products: [
+            .library(
+                    name: "ScanQrKit",
+                    targets: ["ScanQrKit"]),
+        ],
+        dependencies: [
+            .package(url: "https://github.com/horizontalsystems/ComponentKit.Swift.git", .upToNextMajor(from: "1.0.0")),
+            .package(url: "https://github.com/horizontalsystems/ThemeKit.Swift.git", .upToNextMajor(from: "1.0.0")),
+        ],
+        targets: [
+            .target(
+                    name: "ScanQrKit",
+                    dependencies: [
+                        .product(name: "ComponentKit", package: "ComponentKit.Swift"),
+                        .product(name: "ThemeKit", package: "ThemeKit.Swift"),
+                    ]
+            ),
+        ]
 )
--- README.md
+++ README.md
@@ -1,24 +1,15 @@
-# ScanQrKit
-
-## Requirements
+# ScanQrKit.Swift
 
 ## Installation
 
 ### Swift Package Manager
 
-The [Swift Package Manager](https://swift.org/package-manager/) is a tool for automating the distribution of Swift code
-and is integrated into the `swift` compiler. It is in early development, but ScanQrKit does support its use on
-supported platforms.
-
-Once you have your Swift package set up, adding ScanQrKit as a dependency is as easy as adding it to
-the `dependencies` value of your `Package.swift`.
-
 ```swift
 dependencies: [
-    .package(url: "https://github.com/horizontalsystems/ScanQrKit.git", .upToNextMajor(from: "1.0.0"))
+    .package(url: "https://github.com/horizontalsystems/ScanQrKit.Swift.git", .upToNextMajor(from: "1.0.0"))
 ]
 ```
 
 ## License
 
-ScanQrKit is available under the MIT license. See the LICENSE file for more info.
+`ScanQrKit.Swift` is available under the MIT license. See the LICENSE file for more info.
