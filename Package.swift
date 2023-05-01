e6fa3efa47336d17cd39769d7a2ceda12f12c61e
Ermat <ealymbaev@gmail.com>
2023-05-01 16:50:26 +0600

    Update dependencies

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
                     targets: ["ScanQrKit"]),
         ],
         dependencies: [
-            .package(url: "https://github.com/horizontalsystems/ComponentKit.Swift.git", .upToNextMajor(from: "1.0.0")),
-            .package(url: "https://github.com/horizontalsystems/ThemeKit.Swift.git", .upToNextMajor(from: "1.0.0")),
+            .package(url: "https://github.com/horizontalsystems/ComponentKit.Swift.git", .upToNextMajor(from: "2.0.0")),
+            .package(url: "https://github.com/horizontalsystems/ThemeKit.Swift.git", .upToNextMajor(from: "2.0.0")),
         ],
         targets: [
             .target(
